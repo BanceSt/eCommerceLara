278b19f7e5606f1db45a6d9355dbfc7b13011218
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-06-21 13:22:50 -0300

    breeze

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,8 +6,8 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>@yield("title","Home")</title>
 </head>
-<body>
-    <p class="text-green-500 text-4xl font-extrabold dark:text-white">
+<body class="bg-blue-200">
+    <p class="text-green-500 text-8xl mb-8 text-center font-extrabold">
         {{config("app.name")}}
     </p>
     @yield("content")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require('flowbite/plugin')
-  ],
-}
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+      require('@tailwindcss/forms'),
+      forms
+    ],
+};
